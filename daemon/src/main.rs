46a6b9a95b5a3d2708f1652c645ccf222b6eaa9f
plan9better <plan9better@proton.me>
2025-03-30 21:02:28 +0200

    works, ql

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,7 +1,3 @@
-fn main( ){
-  println!("Hello, world!");
-	let x = 42;
-let y = x; // clippy might warn: 'value assigned to `y` is never used'
-
-
-  }
+fn main() {
+    println!("Hello, world!");
+}
